411f1ea539a586a042020840ed7316397d8ab31d
aoki-h-jp <aoki.hirotaka.biz@gmail.com>
2023-03-15 22:54:45 +0900

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='funding-rate-arbitrage',
+    version="1.0.0",
+    description='A framework to help you easily perform funding rate arbitrage on major centralized cryptocurrency '
+                'exchanges.',
+    install_requires=['ccxt', 'pandas'],
+    author='aoki-h-jp',
+    author_email='aoki.hirotaka.biz@gmail.com',
+    license='MIT'
+)
