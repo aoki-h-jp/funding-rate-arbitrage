5ae4aa708776be2e309b2e16e2dcd6879ec5090c
aoki-h-jp <aoki.hirotaka.biz@gmail.com>
2023-03-16 19:06:37 +0900

    Add display_one_by_one

--- frarb/frarb.py
+++ frarb/frarb.py
@@ -1,9 +1,20 @@
-from logging import getLogger
+"""
+Main class of funding-rate-arbitrage
+"""
+import logging
 import ccxt
+from rich import print
+from rich.logging import RichHandler
 from ccxt import ExchangeError
 import pandas as pd
 
-logger = getLogger(__name__)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(message)s",
+    datefmt="[%X]",
+    handlers=[RichHandler(rich_tracebacks=True)]
+)
+log = logging.getLogger("rich")
 
 
 class FundingRateArbitrage:
@@ -32,7 +43,7 @@ class FundingRateArbitrage:
             try:
                 fr_d[p] = ex.fetch_funding_rate(p)['fundingRate']
             except ExchangeError:
-                logger.exception(f'{p} is not perp.')
+                log.exception(f'{p} is not perp.')
         return fr_d
 
     def display_large_divergence_single_exchange(self, exchange: str, minus=False, display_num=10) -> pd.DataFrame:
@@ -44,6 +55,41 @@ class FundingRateArbitrage:
             display_num (int): Number of display.
 
         Returns (pd.DataFrame): DataFrame sorted by large funding rate divergence.
+
+        """
+        return self.get_large_divergence_dataframe_single_exchange(exchange=exchange, minus=minus)\
+            .sort_values(by='Funding Rate [%]', ascending=minus).head(display_num)
+
+    def display_large_divergence_multi_exchange(self, display_num=10, sorted_by='revenue') -> pd.DataFrame:
+        """
+        Display large funding rate divergence between multi CEX.
+        "multi CEX" refers to self.exchanges.
+        Args:
+            display_num (int): Number of display.
+            sorted_by (str): Sorted by "revenue" or "divergence"
+
+        Returns (pd.DataFrame): DataFrame sorted by large funding rate divergence.
+
+        """
+        if sorted_by == 'revenue':
+            sorted_by = 'Revenue [/100 USDT]'
+        elif sorted_by == 'divergence':
+            sorted_by = 'Divergence [%]'
+        else:
+            log.error(f'{sorted_by} is not available.')
+            raise KeyError
+
+        return self.get_large_divergence_dataframe_multi_exchanges()\
+            .sort_values(by=sorted_by, ascending=False).head(display_num)
+
+    def get_large_divergence_dataframe_single_exchange(self, exchange: str, minus=False):
+        """
+        Get large funding rate divergence on single CEX.
+        Args:
+            exchange (str): Name of exchange (binance, bybit, ...)
+            minus (bool): Sorted by minus FR or plus FR.
+
+        Returns (pd.DataFrame): large funding rate divergence DataFrame.
 
         """
         fr = self.fetch_all_funding_rate(exchange=exchange)
@@ -65,36 +111,23 @@ class FundingRateArbitrage:
         df = pd.concat([sr_fr, sr_cm, sr_rv], axis=1)
         df.index = list(fr.keys())
         df.columns = columns
-        return df.sort_values(by=columns[0], ascending=minus).head(display_num)
+        return df
 
-    def display_large_divergence_multi_exchange(self, display_num=10, sorted_by='revenue') -> pd.DataFrame:
+    def get_large_divergence_dataframe_multi_exchanges(self):
         """
-        Display large funding rate divergence between multi CEX.
+        Get large funding rate divergence between multi CEX.
         "multi CEX" refers to self.exchanges.
-        Args:
-            display_num (int): Number of display.
-            sorted_by (str): Sorted by "revenue" or "divergence"
-
-        Returns (pd.DataFrame): DataFrame sorted by large funding rate divergence.
+        Returns (pd.DataFrame): large funding rate divergence DataFrame.
 
         """
-        if sorted_by == 'revenue':
-            sorted_by = 'Revenue [/100 USDT]'
-        elif sorted_by == 'divergence':
-            sorted_by = 'Divergence [%]'
-        else:
-            logger.error(f'{sorted_by} is not available.')
-            raise KeyError
-
         df = pd.DataFrame()
         for ex in self.exchanges:
-            logger.info(f'fetching {ex}')
+            log.info(f'fetching {ex}')
             fr = self.fetch_all_funding_rate(exchange=ex)
             df_ex = pd.DataFrame(fr.values(), index=list(fr.keys()), columns=[ex]).T
             df = pd.concat([df, df_ex])
         df = df.T * 100
 
-        diff_list = []
         diff_d = {}
         for i, data in df.iterrows():
             diff = data.max() - data.min()
@@ -119,7 +152,7 @@ class FundingRateArbitrage:
             else:
                 try:
                     max_commission = self.get_commission(exchange=max_fr_exchange, trade='options') + \
-                                      self.get_commission(exchange=max_fr_exchange, trade='spot')
+                                     self.get_commission(exchange=max_fr_exchange, trade='spot')
                     min_commission = self.get_commission(exchange=min_fr_exchange, trade='futures')
                 except KeyError:
                     max_commission = self.get_commission(exchange=max_fr_exchange, trade='futures')
@@ -135,7 +168,59 @@ class FundingRateArbitrage:
         df_rv = pd.DataFrame(revenue, index=list(comm_d.keys()), columns=['Revenue [/100 USDT]']).T
         df = pd.concat([df.T, df_rv]).T
 
-        return df.sort_values(by=sorted_by, ascending=False).head(display_num)
+        return df
+
+    def display_one_by_one_single_exchange(self, exchange: str, minus=False, display_num=10):
+        df = self.get_large_divergence_dataframe_single_exchange(exchange=exchange, minus=minus)
+        # TODO: Check perp or spot or options exists on CEX.
+        for i in df.sort_values(by='Funding Rate [%]', ascending=minus).head(display_num).index:
+            print('------------------------------------------------')
+            revenue = df.loc[i]["Revenue [/100 USDT]"]
+            if revenue > 0:
+                print(f'[bold deep_sky_blue1]Revenue: {revenue} / 100USDT[/]')
+            else:
+                print(f'[bold red]Revenue: {revenue} / 100USDT[/]')
+            if minus:
+                print(f'[bold red]SELL: {i} Options[/]')
+                print(f'[bold blue]BUY: {i} Perp[/]')
+            else:
+                print(f'[bold red]SELL: {i} Perp[/]')
+                print(f'[bold blue]BUY: {i} Spot[/]')
+            print(f'Funding Rate: {df.loc[i]["Funding Rate [%]"]:.4f} %')
+            print(f'Commission: {df.loc[i]["Commission [%]"]} %')
+
+    def display_one_by_one_multi_exchanges(self, display_num=10, sorted_by='revenue'):
+        if sorted_by == 'revenue':
+            sorted_by = 'Revenue [/100 USDT]'
+        elif sorted_by == 'divergence':
+            sorted_by = 'Divergence [%]'
+        else:
+            log.error(f'{sorted_by} is not available.')
+            raise KeyError
+        df = self.get_large_divergence_dataframe_multi_exchanges()
+        # TODO: Check perp or spot or options exists on CEX.
+        for i in df.sort_values(by=sorted_by, ascending=False).head(display_num).index:
+            print('------------------------------------------------')
+            revenue = df.loc[i]["Revenue [/100 USDT]"]
+            if revenue > 0:
+                print(f'[bold deep_sky_blue1]Revenue: {revenue:.4f} USDT / 100USDT[/]')
+            else:
+                print(f'[bold red]Revenue: {revenue:.4f} USDT / 100USDT[/]')
+            max_fr_exchange = df.loc[i][:-3].idxmax()
+            min_fr_exchange = df.loc[i][:-3].idxmin()
+            max_fr = df.loc[i][:-3].max()
+            min_fr = df.loc[i][:-3].min()
+            if max_fr > 0 and min_fr > 0:
+                print(f'[bold red]SELL: {max_fr_exchange} {i} Perp (Funding Rate {max_fr:.4f} %)[/]')
+                print(f'[bold blue]BUY: {min_fr_exchange} {i} Spot[/]')
+            elif max_fr > 0 > min_fr:
+                print(f'[bold red]SELL: {max_fr_exchange} {i} Perp (Funding Rate {max_fr:.4f} %)[/]')
+                print(f'[bold blue]BUY: {min_fr_exchange} {i} Perp (Funding Rate {min_fr:.4f} %)[/]')
+            else:
+                print(f'[bold red]SELL: {max_fr_exchange} {i} Options[/]')
+                print(f'[bold blue]BUY: {min_fr_exchange} {i} Perp (Funding Rate {min_fr:.4f} %)[/]')
+            print(f'Divergence: {df.loc[i]["Divergence [%]"]:.4f} %')
+            print(f'Commission: {df.loc[i]["Commission [%]"]:.4f} %')
 
     def get_exchanges(self) -> list:
         """
@@ -192,7 +277,7 @@ class FundingRateArbitrage:
             elif trade == 'options':
                 return 0.02
             else:
-                logger.error(f'{trade} is not available on {exchange}.')
+                log.error(f'{trade} is not available on {exchange}.')
                 raise KeyError
 
         # https://www.bybit.com/ja-JP/help-center/bybitHC_Article?id=360039261154&language=ja
@@ -207,7 +292,7 @@ class FundingRateArbitrage:
             elif trade == 'options':
                 return 0.03
             else:
-                logger.error(f'{trade} is not available on {exchange}.')
+                log.error(f'{trade} is not available on {exchange}.')
                 raise KeyError
 
         # https://www.okx.com/fees
@@ -228,7 +313,7 @@ class FundingRateArbitrage:
                 else:
                     return 0.02
             else:
-                logger.error(f'{trade} is not available on {exchange}.')
+                log.error(f'{trade} is not available on {exchange}.')
                 raise KeyError
 
         # https://www.bitget.com/ja/rate/
@@ -244,7 +329,7 @@ class FundingRateArbitrage:
                 else:
                     return 0.017
             else:
-                logger.error(f'{trade} is not available on {exchange}.')
+                log.error(f'{trade} is not available on {exchange}.')
                 raise KeyError
 
         # https://www.gate.io/ja/fee
@@ -260,7 +345,7 @@ class FundingRateArbitrage:
                 else:
                     return 0.015
             else:
-                logger.error(f'{trade} is not available on {exchange}.')
+                log.error(f'{trade} is not available on {exchange}.')
                 raise KeyError
 
         # https://www.coinex.zone/fees?type=spot&market=normal
@@ -276,5 +361,5 @@ class FundingRateArbitrage:
                 else:
                     return 0.03
             else:
-                logger.error(f'{trade} is not available on {exchange}.')
+                log.error(f'{trade} is not available on {exchange}.')
                 raise KeyError
