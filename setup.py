5cea917757f257b8d300691a3a3377ba60b79c1a
Hirotaka Aoki <113173839+aoki-h-jp@users.noreply.github.com>
2023-08-26 02:11:17 +0900

    fix setup error

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     description='A framework to help you easily perform funding rate arbitrage on major centralized cryptocurrency '
                 'exchanges.',
     install_requires=['ccxt', 'pandas', 'rich', 'matplotlib'],
+    packages=['funding_rate_arbitrage'],
     author='aoki-h-jp',
     author_email='aoki.hirotaka.biz@gmail.com',
     license='MIT'
