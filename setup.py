ddc35b14238fa733bc62e4337ffcdb6a2b94a9e9
aoki-h-jp <113173839+aoki-h-jp@users.noreply.github.com>
2023-11-05 18:38:10 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,10 @@ setup(
     name="funding-rate-arbitrage",
     version="1.2.1",
     description="A framework to help you easily perform funding rate arbitrage on major centralized cryptocurrency "
-    "exchanges.",
+                "exchanges.",
     install_requires=["ccxt", "pandas", "rich", "matplotlib"],
-    packages=["funding_rate_arbitrage"],
+    packages=find_packages(include=["funding_rate_arbitrage*"], exclude=["img"]),
     author="aoki-h-jp",
     author_email="aoki.hirotaka.biz@gmail.com",
     license="MIT",
-    packages=find_packages(include=["frarb"], exclude=["img"]),
 )
